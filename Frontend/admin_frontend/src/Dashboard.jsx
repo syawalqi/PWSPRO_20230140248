65c05c5019e11eec94c6c0dd9c003b802610e651
syawalqi <galihmaulanasyawalqi@gmail.com>
2026-01-11 13:40:50 +0700

    membuat login dan dashboard frontend

---
+++ Frontend/admin_frontend/src/Dashboard.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import axios from "axios";
+
+export default function Dashboard() {
+  const token = localStorage.getItem("admin_token");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const createUser = async () => {
+    try {
+      await axios.post(
+        "http://localhost:3000/api/users",
+        { email, password },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }
+      );
+
+      setMessage("User created successfully");
+    } catch {
+      setMessage("Failed to create user");
+    }
+  };
+
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>Admin Dashboard</h2>
+
+      <h3>Create User</h3>
+
+      <input
+        placeholder="User email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+      <br /><br />
+
+      <input
+        type="password"
+        placeholder="User password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+      <br /><br />
+
+      <button onClick={createUser}>Create User</button>
+
+      {message && <p>{message}</p>}
+    </div>
+  );
+}
